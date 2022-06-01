cd5e5291f7d723001f18de3eb33494520cbf100a
cotixmol <constancio.molinengo@outlook.com>
2022-06-01 19:45:53 -0300

    Proyecto Final Coderhouse JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Ejercicio 1
+
+let nombre = "Homero";
+let apellido = "Simpsons"
+let EDAD = 40;
+const CIUDAD = "Springfield"
+
+let carnet = nombre + " " + apellido + " " + EDAD + " " + CIUDAD;
+
+console.log(carnet);
+
+
