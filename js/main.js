b86add7b0e79dd5cb1fa3eb94a689fe00f8f423d
cotixmol <constancio.molinengo@outlook.com>
2022-06-06 15:02:32 -0300

    Armado de HTML y CCS v0.1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-//Ejercicio 1
-
-let nombre = "Homero";
-let apellido = "Simpsons"
-let EDAD = 40;
-const CIUDAD = "Springfield"
-
-let carnet = nombre + " " + apellido + " " + EDAD + " " + CIUDAD;
-
-console.log(carnet);
-
 
